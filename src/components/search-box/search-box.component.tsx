1a4e3d304e02c0a4674052eef073c8dfc0674419
MarkOfosu <124046783+MarkOfosu@users.noreply.github.com>
2023-06-06 10:10:51 -0700

    merge with main to get typescript functionality

---
+++ src/components/search-box/search-box.component.tsx
@@ -0,0 +1,17 @@
+
+import './search-box.styles.css'
+
+
+const SearchBox  = ({className, placeholder,onChangeHandler}) => {
+    return(
+        <input 
+        className={`search-box ${className}`}
+        type={search}
+         placeholder={placeholder}
+         onChange={(e) => onChangeHandler(e)}
+        />
+    );
+}
+   
+
+export default SearchBox;
