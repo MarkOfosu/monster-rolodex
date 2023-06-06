869d445fc0b5b3b6718dbecf88e8918fde0411ef
MarkOfosu <124046783+MarkOfosu@users.noreply.github.com>
2023-06-05 18:09:42 -0700

    changed search box component to functional component

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,16 +1,15 @@
-import { Component } from "react";
+
 import './search-box.styles.css'
-class SearchBox extends Component  {
-    render(){
-        return(
-            <input 
-            className={`search-box ${this.props.className}`}
-            type={this.props.type}
-             placeholder={this.props.placeholder}
-             onChange={this.props.onChangeHandler}
-            />
-        );
-    }
+const SearchBox  = () => {
+    return(
+        <input 
+        className={`search-box ${this.props.className}`}
+        type={this.props.type}
+         placeholder={this.props.placeholder}
+         onChange={this.props.onChangeHandler}
+        />
+    );
 }
+   
 
 export default SearchBox;
